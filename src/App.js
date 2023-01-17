b39296c499a528d9c9a43239eb1dc7265a04e464
sikku <ms1654922@gmail.com>
2023-01-17 09:16:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import About from "./component/About";
+import Explore from "./component/Explore";
+import Reviews from "./component/Reviews";
+import Faq from "./component/Faq";
+import Footer from "./component/Footer";
+import './index.css'
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/about-us" element={<About></About>} />
+          <Route path="/explore-foods" element={<Explore></Explore>} />
+          <Route path="/reviews" element={<Reviews></Reviews>} />
+          <Route path="/faq" element={<Faq></Faq>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
